38b50de70a352b90f8cdeabede44b651c71e405e
Matias Leal <matiasleal96@hotmail.com>
2023-08-18 23:26:07 -0300

    rutas con nombres implementado

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_items.dart';
 import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
+import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const String name = 'home_screen';
   const HomeScreen({super.key});
 
   @override
@@ -58,7 +60,8 @@ class _CustomListTile extends StatelessWidget {
         //   MaterialPageRoute(builder: (context) => const ButtonsScreen()),
         // );
 
-        context.push(menuItem.link);
+        // context.pushNamed(CardsScreen.name);
+        context.push(menuItem.link)
       },
     );
   }
